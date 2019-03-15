28e7a641f54ab8ee26e3e5edb4928a9443d1a895
harshbhanot24m <sunnypaonta@gmail.com>
2019-03-16 00:35:14 +0530

    "upload"

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express=require('express')
 const cors=require('cors')
 const app=express();
-app.use(cors())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
 const login=require('./routes/login')
 const signUp=require('./routes/signUp')
 const post=require('./routes/post')
 app.use('/login',login);
 app.use('/signUp',signUp);
-app.use('/post',post);
+app.use('/posts',post);
 app.get('/',(req,res)=>{
     res.json({"message":"hy friends"})
 })
--- routes/post.js
+++ routes/post.js
@@ -2,14 +2,35 @@
 const express=require('express')
 const cors=require('cors')
 const route=express();
-route.use(cors())
-route.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+route.use(express.json());
+route.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+var multer=require('multer');
+var store= multer.diskStorage({
+destination:function(req,file,cb){
+  cb(null,'./uploads');
+},
+filename:function(re,file,cb){
+  cb(null,Date.now()+"."+file.originalname);
+}
+});
+
+var upload=multer({storage:store}).single('file');
+route.post('/',function(req,res,next){
+  console.log(req.body.value)
+  upload(req,res,function(err){
+    console.log(req.body.value)
+    if(err){
+      return res.status(501).json({error:err})
+    }
+    console.log('workings')
+    return res.status(200).json({originalname:req.file.originalname,uploadname:req.file.fieldname});
+    
   });
-route.post('/',(req,res)=>{
-    console.log(req.body)
-    res.json("hy")
-})
+});
 module.exports=route;
