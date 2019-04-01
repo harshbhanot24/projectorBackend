b96f8be77a8b0bafaa8f5d7cb40e6bb1152e86fc
harshbhanot24m <sunnypaonta@gmail.com>
2019-04-02 01:39:02 +0530

    heroku 1.0.1

--- Common/Connection.js
+++ Common/Connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
     const uri="mongodb+srv://admin:Archer@123@cluster0-njc4r.mongodb.net/test?retryWrites=true";
     const url="mongodb://localhost:27017/dummyDb"
-    mongoose.connect(url, {useNewUrlParser: true}).
+    mongoose.connect(uri, {useNewUrlParser: true}).
     then(()=>{console.log('connectd')}).catch(err=>console.log(err))
   
     module.exports=mongoose;
