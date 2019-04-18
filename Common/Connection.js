b293881cfc3ecd77a063c05bb9e53f62efc5faa1
harshbhanot24m <sunnypaonta@gmail.com>
2019-04-19 02:08:27 +0530

    deployment change1

--- Common/Connection.js
+++ Common/Connection.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-    const uri="mongodb+srv://admin:Archer@123@cluster0-njc4r.mongodb.net/test?retryWrites=true";
+    const uri="mongodb+srv://admin:Samsung24@cluster0-njc4r.mongodb.net/test?retryWrites=true";
     const url="mongodb://localhost:27017/dummyDb"
     mongoose.connect(uri, {useNewUrlParser: true}).
     then(()=>{console.log('connectd')}).catch(err=>console.log(err))
